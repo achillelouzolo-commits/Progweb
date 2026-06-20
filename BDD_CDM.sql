09f21913b928353fa88900071b377ff2effa0167
achillelouzolo-commits <achille.louzolo@gmail.com>
2026-06-20 15:17:50 +0200

    Premier commit de base de données
    Création des tables (groupes, selections, matchs et stades)

---
+++ BDD_CDM.sql
@@ -0,0 +1,38 @@
+CREATE TABLE Selection (
+    idSelection NUMBER PRIMARY KEY,
+    Nom VARCHAR2(50),
+    Continent VARCHAR2(50),
+    Selectionneur VARCHAR2(50),
+    ClassementFifa NUMBER
+);
+
+CREATE TABLE Groupe (
+    idGroupe NUMBER PRIMARY KEY,
+    NomGroupe VARCHAR2(1)
+);
+
+CREATE TABLE GS (
+    idGroupe NUMBER,
+    idSelection NUMBER,
+    PRIMARY KEY(idGroupe, idSelection),
+    FOREIGN KEY (idSelection) REFERENCES Selection(idSelection),
+    FOREIGN KEY (idGroupe) REFERENCES Groupe(idGroupe)
+);
+
+CREATE TABLE Stade (
+    idStade NUMBER PRIMARY KEY,
+    Ville VARCHAR2(50),
+    Pays VARCHAR2(50),
+    Capacite NUMBER
+);
+
+CREATE TABLE Matchs (
+    idMatch NUMBER PRIMARY KEY,
+    idSelection1 NUMBER,
+    idSelection2 NUMBER,
+    idStade NUMBER,
+    DateMatch DATE,
+    FOREIGN KEY (idSelection1) REFERENCES Selection(idSelection),
+    FOREIGN KEY (idSelection2) REFERENCES Selection(idSelection),
+    FOREIGN KEY (idStade) REFERENCES Stade(idStade)
+);
